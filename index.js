7e2e186161d3304df4ee22d9fcfb0a6a4428d0ea
Sebastian Gaviria Giraldo <1112478378@academico.icesi.edu.co>
2020-02-13 18:57:36 -0500

    type categorization by folder name

--- index.js
+++ index.js
@@ -1,49 +1,74 @@
 const express = require('express');
 const Jimp = require('jimp');
 const assert = require('assert');
+const fs = require('fs');
 
 const app = express();
 
 app.get('/api/1/images/:dimensions', function(req, res){ 
-    Jimp.read('./public/img/Cat_November_2010-1a.jpg', function(err, img){
+    var type = req.query.type;
+
+    fs.readdir('./public/img/', function(err, files){
         assert.equal(err, null);
 
-        const size = req.params.dimensions.split('x');
-        const width = parseInt(size[0]);
-        const height = parseInt(size[1]);
+        const typeExists = files.includes(type);
+
+        if(!typeExists){
+            // creo un número random desde 0 hasta el tamaño del arreglo
+            const random = Math.floor(Math.random() * files.length);
+            // uso el número random para sacar la carpeta en esa posición
+            type = files[random];
+        }
+
         
-        const resizeType = req.query.resize_type;
-        const bg = req.query.bg;
-        switch(resizeType){
-            case 'contain':
-                img.background(parseInt('0x' + bg + 'ff'));
-                img.contain(width, height);
-                break;
-            case 'stretch':
-                img.resize(width, height);
-                break;
-            case 'cover':
-            default:
-                img.cover(width, height);
-        }
+        fs.readdir('./public/img/' + type, function(err, files){
+            assert.equal(err, null);
+            console.log(files);
+            // creo un número random desde 0 hasta el tamaño del arreglo
+            const random = Math.floor(Math.random() * files.length);
+            // uso el número random para sacar el nombre de la imagen en esa posición
+            imgName = files[random];
 
-        const text = req.query.text;
-        if(text){
-            Jimp.loadFont(Jimp.FONT_SANS_64_BLACK).then(function (font) {
-                img.print(font, 10, 10, text);
-                sendImage();
+            Jimp.read('./public/img/' + type + '/' + imgName, function(err, img){
+                assert.equal(err, null);
+        
+                const size = req.params.dimensions.split('x');
+                const width = parseInt(size[0]);
+                const height = parseInt(size[1]);
+                
+                const resizeType = req.query.resize_type;
+                const bg = req.query.bg;
+                switch(resizeType){
+                    case 'contain':
+                        img.background(parseInt('0x' + bg + 'ff'));
+                        img.contain(width, height);
+                        break;
+                    case 'stretch':
+                        img.resize(width, height);
+                        break;
+                    case 'cover':
+                    default:
+                        img.cover(width, height);
+                }
+        
+                const text = req.query.text;
+                if(text){
+                    Jimp.loadFont(Jimp.FONT_SANS_64_BLACK).then(function (font) {
+                        img.print(font, 10, 10, text);
+                        sendImage();
+                    });
+                } else {
+                    sendImage();
+                }
+        
+                function sendImage(){
+                    img.getBuffer(Jimp.MIME_JPEG, function(err, buffer){
+                        res.set("Content-Type", Jimp.MIME_JPEG);
+                        res.send(buffer);
+                    });
+                }
             });
-        } else {
-            sendImage();
-        }
-
-        function sendImage(){
-            img.getBuffer(Jimp.MIME_JPEG, function(err, buffer){
-                res.set("Content-Type", Jimp.MIME_JPEG);
-                res.send(buffer);
-            });
-        }
-
+        });
     });
 });
 
